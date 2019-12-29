a48315c2fed3bfa2085cb5b50eccda2d8e08f307
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2019-12-29 23:51:32 +0100

    Add PCB class properties and public State enum, Add setters and getters in PCB class

--- os-simulator/PCB.cpp
+++ os-simulator/PCB.cpp
@@ -1,12 +1,71 @@
 #include "pch.h"
 #include "PCB.h"
 
-
-PCB::PCB()
+PCB::PCB(const std::string& name, const std::string& file_name, const int priority, const int id) : name_(name), file_name_(file_name), id_(id), priority_(priority), registers_{}
 {
 }
 
-
-PCB::~PCB()
+std::string PCB::name() const
 {
+	return name_;
+}
+
+void PCB::set_name(const std::string& name)
+{
+	name_ = name;
+}
+
+std::string PCB::file_name() const
+{
+	return file_name_;
+}
+
+void PCB::set_file_name(const std::string& file_name)
+{
+	file_name_ = file_name;
+}
+
+int PCB::process_state() const
+{
+	return process_state_;
+}
+
+void PCB::set_process_state(State process_state)
+{
+	process_state_ = process_state;
+}
+
+int PCB::id() const
+{
+	return id_;
+}
+
+int PCB::priority() const
+{
+	return priority_;
+}
+
+void PCB::set_priority(int priority)
+{
+	priority_ = priority;
+}
+
+int PCB::instruction_counter() const
+{
+	return instruction_counter_;
+}
+
+void PCB::set_instruction_counter(int instruction_counter)
+{
+	instruction_counter_ = instruction_counter;
+}
+
+std::vector<Segment> PCB::segment_tab() const
+{
+	return segment_tab_;
+}
+
+void PCB::set_segment_tab(const std::vector<Segment>& segment_tab)
+{
+	segment_tab_ = segment_tab;
 }
--- os-simulator/PCB.h
+++ os-simulator/PCB.h
@@ -1,13 +1,43 @@
 #pragma once
 #include<vector>
 #include"Segment.h"
+#include <string>
+
 class PCB
 {
 public:
-	PCB();
+	enum State
+	{
+		Waiting,
+		Ready,
+		Running,
+	};
+private:
+	std::string name_;
+	std::string file_name_;
+	int process_state_{};
+	int id_{};
+	int priority_;
+	int registers_[3];
+	int instruction_counter_{};
+	std::vector<Segment> segment_tab_; // TODO: Consider changing its name
+public:
+	PCB(const std::string& name, const std::string& file_name, int priority, int id);
 	~PCB();
-	std::vector<Segment> segment_tab;
-	std::string file_name;
 
+	std::string name() const;
+	std::string file_name() const;
+	int process_state() const;
+	int id() const;
+	int priority() const;
+	int instruction_counter() const;
+	std::vector<Segment> segment_tab() const;
+	
+	void set_name(const std::string& name);
+	void set_file_name(const std::string& file_name);
+	void set_process_state(State process_state);
+	void set_priority(int priority);
+	void set_instruction_counter(int instruction_counter);
+	void set_segment_tab(const std::vector<Segment>& segment_tab);
 };
 
