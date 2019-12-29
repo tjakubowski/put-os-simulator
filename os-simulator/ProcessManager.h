7a49c84ae387c29497ce1bdb937e88ff5a6f7079
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2019-12-29 22:17:16 +0100

    Add constructor and methods for moving process to the right lists in ProcessManager

--- os-simulator/ProcessManager.h
+++ os-simulator/ProcessManager.h
@@ -11,10 +11,26 @@ class ProcessManager
 	PCB* running_process_ = nullptr;
 	PCB* dummy_process_ = nullptr;
 public:
+
+	ProcessManager() = default;
 	void CreateProcess(std::string process_name, std::string process_file);
-	void KillProcess(PCB process);
-	void KillProcess(std::string process_name);
+
+	void KillProcess(PCB* process);
 	void KillProcess(int process_id);
-	PCB GetProcess(std::string process_name);
-	PCB GetProcess(int process_id);
+	void KillProcess(std::string process_name);
+	
+	PCB* GetProcess(int process_id);
+	PCB* GetProcess(std::string process_name);
+
+	void SetProcessRunning(PCB* process);
+	void SetProcessRunning(int process_id);
+	void SetProcessRunning(std::string process_name);
+	
+	void SetProcessReady(PCB* process);
+	void SetProcessReady(int process_id);
+	void SetProcessReady(std::string process_name);
+	
+	void SetProcessWaiting(PCB* process);
+	void SetProcessWaiting(int process_id);
+	void SetProcessWaiting(std::string process_name);
 };
