0c8fea86cfbee3eb6fafbfbbc793d3cb5cdc0243
ggEx86 <mxbalcewicz@gmail.com>
2019-12-29 18:08:18 +0100

    Update CPU.cpp file structure

--- os-simulator/CPU.cpp
+++ os-simulator/CPU.cpp
@@ -1,8 +1,22 @@
 #include "CPU.h"
 
 
+void order_heap(Process Heap[], int* heapsize, int start){
 
-int main(){
+}
 
+void insert_to_heap(Process Heap[], int* heapsize, int priority){
 
+}
+
+void scheduling(Process Heap[], int* heapsize, int n, int* heapsize){
+
+}
+
+bool compare(Process P1, Process P2){
+
+}
+
+Process extract_highest(Process Heap[], int* heapsize){
+    
 }
