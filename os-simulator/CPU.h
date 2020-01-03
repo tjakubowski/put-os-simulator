5bf0f4f14d8f3e02cad3730f899146d717192d09
ggEx86 <mxbalcewicz@gmail.com>
2020-01-03 05:00:19 +0100

    Update files' structures, misspells

--- os-simulator/CPU.h
+++ os-simulator/CPU.h
@@ -10,18 +10,13 @@ class CPU_M{
 
     //Utrzymanie kopca
 	void order_heap(std::vector<Process*> &heap);
-    
-	
    
 	//WYkonywanie procesu na gorze kopca
-    void scheduling(std::vector <Process> Heap, int* heapsize);
+    void scheduling(std::vector <Process*> &heap);
     
 	//Porownanie procesow pod wzgledem priority
 	bool compare(Process *P1, Process *P2);
 
-
     //Wyciagniecie ze stosu procesu z najwiekszym priorytetem
-    Process extract_highest(std::vector <Process> Heap, int* heapsize);
-    
-
+    Process extract_highest(std::vector <Process*> &heap);
 };
