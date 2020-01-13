f21f883d65cfdce533240e845289d9df5bfc0f71
Dawid Bronszkiewicz <dbronszkiewicz@gmail.com>
2020-01-13 19:25:39 +0100

    Uporządkowanie poleceń oraz dodanie funkcji sprawdzającej zmiany w statusie procesu running

--- os-simulator/main.cpp
+++ os-simulator/main.cpp
@@ -1,10 +1,20 @@
 #include "pch.h"
 #include <iostream>
 #include "Shell.h"
+#include "FileM.h"
+#include "RAM.h"
+
+FileM disc;
+RAM ram;
+
+//PCB *p1 = new PCB(1);
+int change_state = 0;
+std::string code;
 
 int main()
 {
 	Shell shell;
+
 
 	std::cout << "OS Simulator" << std::endl;
 
