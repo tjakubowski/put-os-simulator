6d2bf7ab111e4bf176ab297a6019943b0572a80c
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-01-15 22:31:49 +0100

    Add TablePrinter class used to print data in well aligned table

---
+++ os-simulator/TablePrinter.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "TablePrinter.h"
+
+TablePrinter::TablePrinter(std::string column_separator, int column_padding) : column_separator_(column_separator), column_padding_(column_padding)
+{
+}
+
+int TablePrinter::GetTotalWidth()
+{
+	const int separators_total_size = column_separator_.size() * (1 + columns_.size());
+	const int paddings_total_size = column_padding_ * columns_.size() * 2;
+	int columns_total_size = 0;
+
+	for (auto& column : columns_)
+		columns_total_size += column.column_width_;
+
+	return separators_total_size + paddings_total_size + columns_total_size;
+}
+
+void TablePrinter::PrintLine()
+{
+	std::cout << std::string(GetTotalWidth(), '-') << std::endl;
+}
+
+void TablePrinter::AddColumn(std::string column_name, unsigned int column_width, bool column_align_left)
+{
+	column_width = std::max(column_width, column_name.size());
+	columns_.push_back({ column_name, column_width, column_align_left });
+}
+
+void TablePrinter::PrintHeader()
+{
+	PrintLine();
+
+	for (auto& column : columns_)
+	{
+		std::cout
+			<< column_separator_ << std::string(column_padding_, ' ');
+		if (column.column_align_left_)
+			std::cout << std::left;
+		else
+			std::cout << std::right;
+		std::cout << std::setw(column.column_width_) << column.column_name_ << std::string(column_padding_, ' ');
+	}
+		
+	std::cout << column_separator_ << std::endl;
+	
+	PrintLine();
+}
+
+void TablePrinter::PrintFooter()
+{
+	PrintLine();
+}
---
+++ os-simulator/TablePrinter.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+class TablePrinter
+{
+	struct TableColumn
+	{
+		std::string column_name_;
+		unsigned int column_width_;
+		bool column_align_left_;
+	};
+	
+	unsigned int input_counter_ = 0;
+	std::vector<TableColumn> columns_;
+	std::string column_separator_;
+	unsigned int column_padding_;
+
+	int GetTotalWidth();
+	void PrintLine();
+
+public:
+	TablePrinter(std::string column_separator = "|", int column_padding = 1);
+	void AddColumn(std::string column_name, unsigned int column_width, bool column_align_left = true);
+	void PrintHeader();
+	void PrintFooter();
+
+	template<typename T> TablePrinter& operator<<(T input)
+	{
+		auto column = columns_[input_counter_];
+
+		std::stringstream input_string_stream;
+		input_string_stream << input;
+		std::string input_string = input_string_stream.str();
+
+		if (input_string.length() > column.column_width_)
+			input_string = input_string.substr(0, column.column_width_ - 1) + "*";
+		
+		std::cout
+			<< column_separator_ << std::string(column_padding_, ' ');
+		
+		column.column_align_left_ ? std::cout << std::left : std::cout << std::right;
+		
+		std::cout << std::setw(column.column_width_) << input_string << std::string(column_padding_, ' ');
+
+		if (++input_counter_ == columns_.size())
+		{
+			std::cout << column_separator_ << std::endl;
+			input_counter_ = 0;
+		}
+
+		return *this;
+	}
+
+};
+
