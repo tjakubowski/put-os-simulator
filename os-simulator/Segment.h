1894b7cac34c3e2827b78d2c5a5586994f85ae04
Kamil Mosek <kamil.mosek@gmail.com>
2020-01-17 16:33:20 +0100

    add string data to Segment update createProgram and load_to_virtualMemory function in VirtualMemory

--- os-simulator/Segment.h
+++ os-simulator/Segment.h
@@ -2,10 +2,11 @@
 class Segment
 {
 public:
+	std::string data;
 	bool is_in_RAM;
 	int baseVM, baseRAM;
 	int limit;//rozmiar 
-	Segment(int base, int limit, bool is_in_RAM);
+	//Segment(int base, int limit, bool is_in_RAM);
 	Segment();
 	~Segment();
 };
