ae3db900df7a19eb160c6c1bde39d95c695c6afd
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2020-01-22 17:25:15 +0100

    update interfejs krokowy

--- os-simulator/Semaphore.cpp
+++ os-simulator/Semaphore.cpp
@@ -46,7 +46,7 @@ void Semaphore::show_Semaphore()
 	table_printer.AddColumn("Aktualny stan Semaphora", 3);
 	
 	table_printer.PrintHeader();
-	table_printer << value;
+	table_printer << "\t" <<value;
 	table_printer.PrintFooter();
 
 	table_printer.ClearColumns();
@@ -59,10 +59,10 @@ void Semaphore::show_Semaphore()
 	{
 		table_printer.AddColumn("Aktualny rozmiar kolejki", 2);
 		table_printer.AddColumn("poczatek kolejki", 2);
-		table_printer.AddColumn("koniec kolejki", 2);
+
 		table_printer.PrintHeader();
 
-		std::cout << queue.size() << queue.front() << queue.back() << std::endl;
+		std::cout << queue.size() << queue.front()->file_name() << std::endl;
 
 		table_printer.PrintFooter();
 	}
